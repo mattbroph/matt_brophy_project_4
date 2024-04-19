74ab3c1945ecf661b05cc68a4c4ad5ac16baf125
Matt <mattbroph@gmail.com>
2024-04-19 14:12:17 -0500

    updated src/App.js for project 4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World! It's project 4!
+          Hello World! This is Matt!
         </p>
         <a
           className="App-link"
